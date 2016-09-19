9af3ba22b6938cfdeaa5da037aded6f35da88dde
Julian Galvis <jgalvis@alertlogic.com>
2016-09-18 22:13:23 -0500

    Step 3 DataBase Connections

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
   "fmt"
   "net/http"
   "html/template"
+
+  "database/sql"
+  _ "github.com/mattn/go-sqlite3"
 )
 //Struct Page
 type Page struct {
   Name string
+  DBStatus bool
 }
 //Function main
 func main() {
   //Initialize and Parse the template
   templates := template.Must(template.ParseFiles("templates/index.html"))
+  //establish conection with the DB
+  db, _ := sql.Open("sqlite3", "dev.db")
   //Stating the route
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     //New instance of page
@@ -22,6 +28,7 @@ func main() {
       //Assing value from get
       p.Name = name
     }
+    p.DBStatus = db.Ping() == nil
     //Execute the template
     if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
       //Alert the user something is wrong
